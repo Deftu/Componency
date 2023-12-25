eaffca3bbc73f5edbc4ed23910cfc3f14a2e32f4
Deftu <deftudev@gmail.com>
2023-12-25 18:28:25 +0200

    Delete old code.

--- src/main/kotlin/dev/deftu/componency/dsl/color.kt
+++
@@ -1,8 +0,0 @@
-package dev.deftu.componency.dsl
-
-import java.awt.Color
-
-fun Color.withRed(red: Int) = Color(red, green, blue, alpha)
-fun Color.withGreen(green: Int) = Color(red, green, blue, alpha)
-fun Color.withBlue(blue: Int) = Color(red, green, blue, alpha)
-fun Color.withAlpha(alpha: Int) = Color(red, green, blue, alpha)
