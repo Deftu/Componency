ebc536961867bfae92fd315cffae0111060cb22b
Deftu <me@deftu.dev>
2024-08-20 15:19:15 +0200

    Return Minecraft-specific engine implementations in MinecraftEngine

--- minecraft/src/main/kotlin/dev/deftu/componency/minecraft/MinecraftEngine.kt
+++ minecraft/src/main/kotlin/dev/deftu/componency/minecraft/MinecraftEngine.kt
@@ -1,13 +1,11 @@
 package dev.deftu.componency.minecraft
 
 import dev.deftu.componency.engine.Engine
-import dev.deftu.componency.engine.InputEngine
-import dev.deftu.componency.engine.RenderEngine
 
 public object MinecraftEngine : Engine() {
 
-    override val renderEngine: RenderEngine = MinecraftRenderEngine
+    override val renderEngine: MinecraftRenderEngine = MinecraftRenderEngine
 
-    override val inputEngine: InputEngine = MinecraftInputEngine
+    override val inputEngine: MinecraftInputEngine = MinecraftInputEngine
 
 }
