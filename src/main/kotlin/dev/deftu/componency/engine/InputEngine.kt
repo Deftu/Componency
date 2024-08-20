6d830a7e6bb51e35e7faaaeae6331d77e4bb7707
Deftu <me@deftu.dev>
2024-08-20 15:14:47 +0200

    Add `isMouseButtonDown` and `isKeyDown` to input engine

--- src/main/kotlin/dev/deftu/componency/engine/InputEngine.kt
+++ src/main/kotlin/dev/deftu/componency/engine/InputEngine.kt
@@ -1,9 +1,16 @@
 package dev.deftu.componency.engine
 
+import dev.deftu.componency.input.Key
+import dev.deftu.componency.input.MouseButton
+
 public interface InputEngine {
 
     public val mouseX: Float
 
     public val mouseY: Float
+
+    public fun isMouseButtonDown(button: MouseButton): Boolean
+
+    public fun isKeyDown(key: Key): Boolean
 
 }
